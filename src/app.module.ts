f7f4bca8a893d607c632a95048430cdcad343481
Tony Thai <trungthaihieu93.dedrdo@gmail.com>
2022-05-27 12:26:39 +0700

    Add libs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { OtpModule } from './otp/otp.module';
+import { EmailModule } from './email/email.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, OtpModule, EmailModule],
   controllers: [AppController],
   providers: [AppService],
 })
