f2245fed12cb6efb89d4a38e907731fa2b344dd4
Tony Thai <trungthaihieu93.dedrdo@gmail.com>
2022-05-27 23:17:56 +0700

    Change 12 hour type to 24 hour type

--- src/utils/datetime.ts
+++ src/utils/datetime.ts
@@ -1,6 +1,6 @@
 import * as moment from 'moment';
 
 export const parseToMySqlDatetime = (datetime: Date) =>
-  moment(datetime).format('YYYY-MM-DD hh:mm:ss');
+  moment(datetime).format('YYYY-MM-DD HH:mm:ss');
 
 export const parseMySqlDateToISO = (datetime: string) => {};
